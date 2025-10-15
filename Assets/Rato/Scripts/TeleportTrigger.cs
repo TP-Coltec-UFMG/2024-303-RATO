90775c2c7342e087c57a742ca07d40b1553776b2
joana4321 <161602331+joana4321@users.noreply.github.com>
2025-10-15 13:13:15 -0300

    cooldown na subida e descida de escada e mudança do aspect ratio para 16:9

--- Assets/Rato/Scripts/TeleportTrigger.cs
+++ Assets/Rato/Scripts/TeleportTrigger.cs
@@ -8,24 +8,40 @@ public class TeleportTrigger : MonoBehaviour
     [SerializeField] private Transform TeleportThis;
     [SerializeField] private Vector3 NewPosition;
 
+    [Header("Controle de Cooldown")]
+    [Tooltip("Tempo em segundos para prevenir ativações duplas após o teleport.")]
+    public float cooldownTempo = 0.2f; 
+    private static float proximaAcaoTempo = 0f;
+
     void OnTriggerEnter2D(Collider2D collider){
-        if(collider.gameObject.tag == "Player"){
-            InteragirTutorial.SetActive(true);
-            StartCoroutine(WaitForKeyPress());
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            if (Time.time >= proximaAcaoTempo)
+            {
+                InteragirTutorial.SetActive(true);
+                StartCoroutine(WaitForKeyPress());
+            }
         }
     }
 
-    void OnTriggerExit2D(){
-        InteragirTutorial.SetActive(false);
-        StopAllCoroutines();
+    void OnTriggerExit2D(Collider2D collider){
+        if (GetComponent<Collider>().gameObject.CompareTag("Player"))
+        {
+            InteragirTutorial.SetActive(false);
+            StopAllCoroutines();
+        }
     }
 
     private IEnumerator WaitForKeyPress(){
-        while(!UserInput.Instance.InteractInput){
+        while (!UserInput.Instance.InteractInput) {
             yield return null;
         }
-        
-        Teleport();
+
+        if (Time.time >= proximaAcaoTempo)
+        {
+            Teleport();
+            proximaAcaoTempo = Time.time + cooldownTempo; 
+        }
     }
 
     private void Teleport(){
